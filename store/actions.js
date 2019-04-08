6ad63c87666b83dd6fb7fb39a712673c10610e0a
andmorefine <andmorefine@gmail.com>
2019-04-09 01:33:59 +0900

    add

--- store/actions.js
+++ store/actions.js
@@ -1,9 +1,8 @@
 export default {
   async getContent({ state, commit }) {
     commit('increment')
-    console.log(state.counter)
     const content = await this.$axios.$get(
-      `/api/v1/one_hundred_people/${state.counter}`
+      `https://api.andmorefine.com/api/v1/one_hundred_people/${state.counter}`
     )
     commit('insert', content)
   },
